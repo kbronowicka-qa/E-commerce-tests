3308f4d32c18e7641d01fc9b7e84b53b748e8c26
Karolina Bronowicka <strangenessxandxcharm@gmail.com>
2026-03-01 20:37:38 +0100

    Add tests for products page

--- cypress/fixtures/global/pages.js
+++ cypress/fixtures/global/pages.js
@@ -2,4 +2,6 @@ export const globalPages = {
     signUpLogin: 'a[href="/login"]',
     logout: 'a[href="/logout"]',
     deleteAccount: 'a[href="/delete_account"]',
+    products: 'a[href="/products"]',
+    cart: 'a[href="/view_cart"]',
 }
